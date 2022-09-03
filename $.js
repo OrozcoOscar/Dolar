0e54db7b324d6e9e44b6f9e4cb700a1e526f5527
Orozco Oscar <88407386+OrozcoOscar@users.noreply.github.com>
2022-09-03 10:51:38 -0500

    Update $.js
    Se añadió comentarios a la gran mayoría de las funciones

--- $.js
+++ $.js
@@ -1,9 +1,12 @@
 /**
 OrozcoOscar
-v4.10
-30/08/22
+v4.12
+3/09/22
 **/
-
+/**
+ * Función para facilitar el manejo del DOM
+ * @param {string} argument Selector Css (".micaja","#midiv","body",etc..)
+ */
 function $(argument) {
 	class obj {
 		constructor(e) {
@@ -14,33 +17,68 @@ function $(argument) {
 			}
 			else{ this.tag = e[0] || e;}
 		}
+		/**
+		 * Retorna o modifica el contenido de uno o mas elementos HTML
+		 * @param {string|undefined} e Contenido a modificar
+		 * @returns innerHTML
+		 */
 		html(e) {
 			if (!e) return this.tag.innerHTML;
 			if (this.tag.length > 1)
 				for (var t = 0; t < this.tag.length; t++) this.tag[t].innerHTML = e;
 			else this.tag.innerHTML = e
 		}
-		event(e, t) {
+		/**
+		 * Añade eventos a uno o mas elementos HTML; 
+		 * @param {string} e Nombre del evento "click" ,"key" etc
+		 * @param {function} f función que ejecuta el evento $(selectorCss).event("click",myFuncion)
+		 * la función recibe como parámetro los datos del evento
+		 */
+		event(e, f) {
 			if (this.tag.length > 1)
-				for (var n = 0; n < this.tag.length; n++) this.tag[n].addEventListener(e, t);
-			else this.tag.addEventListener(e, t)
+				for (var n = 0; n < this.tag.length; n++) this.tag[n].addEventListener(e, f);
+			else this.tag.addEventListener(e, f)
 		}
-		removeEvent(e, t) {
+		/**
+		 * Quita eventos a uno o mas elementos HTML
+		 * @param {string} e Nombre del evento "click" ,"key" etc
+		 * @param {function} f función que ejecuta el evento $(selectorCss).removeEvent("click",myFuncion)
+		 */
+		removeEvent(e, f) {
 			if (this.tag.length > 1)
-				for (var n = 0; n < this.tag.length; n++) this.tag[n].removeEventListener(e, t);
-			else this.tag.removeEventListener(e, t)
+				for (var n = 0; n < this.tag.length; n++) this.tag[n].removeEventListener(e, f);
+			else this.tag.removeEventListener(e, f)
 		}
+		/**
+		 * Retorna o modifica el value de un elemento HTML
+		 * @param {string} e 
+		 * @returns value
+		 */
 		val(e) {
 			return e ? this.tag.value = e : this.tag.value
 		}
+		/**
+		 * Retorna o modifica el src de un elemento HTML
+		 * @param {string} e 
+		 * @returns src
+		 */
 		src(e) {
 			return e ? this.tag.src = e : this.tag.src
 		}
+		/**
+		 * Inserta Atributos a uno o varios elementos HTML
+		 * @param {string} e Nombre del Atributo
+		 * @param {string} t Valor del Atributo
+		 */
 		attr(e, t) {
 			if (this.tag.length > 1)
 				for (var n = 0; n < this.tag.length; n++) this._current[n].attr(e, t);
 			else this.tag.setAttribute(e, t)
 		}
+		/**
+		 * Inserta contenido al final a uno o varios elementos HTML
+		 * @param {string} e contenido
+		 */
 		append(e) {
 			try {
 				if (this.tag.length > 1)
@@ -48,6 +86,10 @@ function $(argument) {
 				else this.tag.innerHTML += e
 			} catch (e) {}
 		}
+		/**
+		 * Inserta Css a uno o varios elementos HTML
+		 * @param {Json} obj estilos Css
+		 */
 		css(obj) {
 			if (this.tag.length > 1)
 				for (var i = 0; i < this.tag.length; i++)
@@ -55,11 +97,19 @@ function $(argument) {
 			else
 				for (let p in obj) this.tag.style[p]=obj[p]
 		}
+		/**
+		 * Inserta clase a uno o varios elementos HTML
+		 * @param {string} e nombre de la clase
+		 */
 		toggleClass(e) {
 			if (this.tag.length > 1)
 				for (var t = 0; t < this.tag.length; t++) this.tag[t].classList.toggle(e);
 			else this.tag.classList.toggle(e)
 		}
+		/**
+		 * Quita clase a uno o varios elementos HTML
+		 * @param {string} e nombre de la clase
+		 */
 		removeClass(e) {
 			if (this.tag.length > 1)
 				for (var t = 0; t < this.tag.length; t++) this.tag[t].classList.remove(e);
@@ -75,20 +125,32 @@ function $(argument) {
 	else return undefined
 }
 /**
-     * Resolver Ecuaciones lineales 
+     * Calcula la Distancia entre dos puntos 
      * @param p1 primer punto.
      * @param p2  segundo punto.
      */
 function distanceBetweenPoints(p1={x:0,y:0},p2={x:0,y:0}){
 	return Math.sqrt((p1.x-p2.x)**2+(p1.y-p2.y)**2)
 }
+/**
+ * Crea una Matriz
+ * @param {number} f N Filas
+ * @param {number} c N Columnas
+ * @param {any} r Relleno
+ * @returns Una Matriz
+ */
 function createMatriz(f,c,r=0) {let m=[f];for (var i = 0; i <f; i++) {m[i]=[];for (var e = 0; e < c; e++) {m[i][e]=r;}}return m;}
 function Random(min, max) { return Math.floor(Math.random() * (max - min)) + min;}//no incluye al max
+/**
+ * 
+ * @returns {Json} Retorna un json con los parámetros obtenidos de la url o método GET
+ */
 function Get() {let cont=window.location.search;if(cont.indexOf("=")>-1){let json="{";let get=cont.replace("?","");get=get.split("&");get.map((e,i)=>{e=e.split("=");if(i<get.length-1)json+="\""+e[0]+"\":\""+e[1].replace(/%20/g," ")+"\",";else json+="\""+e[0]+"\":\""+e[1].replace(/%20/g," ")+"\""+"}";});return JSON.parse(json);}else return null;}
 /**
      * Resolver Ecuaciones lineales 
      * @param M Matriz de coeficientes.
      * @param equality  Array de igualdades.
+	 * @return "Array" con los resultados (null o undefine es que no tiene solución )
 	 * -----------------------------------
 	 * Ej: M=[[2,1],[5,2]] equality=[10,10]
 	 *  es equivalente  a 2x+y=10 ; 5x+2y=10
@@ -166,7 +228,7 @@ const toRad=(g)=>g*Math.PI/180
 const toGrad=(r)=>r*180/Math.PI
 const binToASCII=(bin)=>bin.map(b=>parseInt(b,2).toString(10))//bin:[]
 const numToBin=(num)=>num.toString(2)
-const asciiToText=(ascii)=>String.fromCharCode(...ascii)//ascii:int[]
+const asciiToText=(ascii)=>String.fromCharCode(...ascii)//ascii:number[]
 const textToAscii=(text)=>text.split("").map(c=>c.charCodeAt(0))
 const textToBin=(text)=>textToAscii(text).map(c=>numToBin(c))
 const binToNum=(bin)=>parseInt(bin,2);
@@ -233,6 +295,10 @@ class Canvas{
 		if(this.tag)this.ctx=this.tag.getContext("2d");
 		else console.error("NO se encuentra un elemnto canvas");
 	}
+	/**
+	 * Limpia el canvas
+	 * @param {Array} n Array de 3 posiciones [r,g,b] donde r,g,b son números entre 0 y 255
+	 */
 	clear(n){
 		if(n){
 			this.rect(0,0,this.tag.width,this.tag.height,"rgb("+n+")",true)
@@ -244,6 +310,15 @@ class Canvas{
 	getCanvas(){
 		return $("canvas");
 	}
+	/**
+	 * Pinta un poligono de n lados
+	 * @param {number} x Posición en x
+	 * @param {number} y Posición en x
+	 * @param {number} n Numero de lados
+	 * @param {number} radio 
+	 * @param {number|undefined} angulo 
+	 * @param {boolean} f Relleno o no relleno por defecto esta en false
+	 */
 	polygon(x,y, n, radio, angulo=undefined,f=false){
             let incremento= 360/n;
             let vx=[], vy=[];
@@ -268,9 +343,17 @@ class Canvas{
             this.ctx.stroke();
 			if(f)this.ctx.fill();
         }
+		/** Pinta un rectángulo
+		 * @param {number} x Posición en x
+		 * @param {number} y Posición en x
+		 * @param {number} w Ancho
+		 * @param {number} h Alto
+		 * @param {string} c Color
+	     * @param {boolean} f Relleno o no relleno por defecto esta en false
+		 * @param {number|Array} r Recibe un numero o un array de 4 posiciones (valores de redondez de las esquinas)
+		 */
 	rect(x,y,w,h,c,f=false,r=false){
-	//x:int y:int w:int h:int c:String(color) f:bool(relleno) r:int,array[4]:int (valores de redondes de las esquinas)
-		if(r){
+	if(r){
 			if(Number(r)){
 				this.ctx.beginPath()
 				this.ctx.save();
@@ -308,6 +391,14 @@ class Canvas{
 		}
 	
 	}
+	/**
+	 * Pinta un circulo
+ 	 * @param {number} x Posición en x
+	 * @param {number} y Posición en x
+	 * @param {number} r Radio
+	 * @param {string} c Color
+	 * @param {boolean} f Relleno o no relleno por defecto esta en false
+	 */
 	circle(x,y,r,c,f=false){
 		this.ctx.beginPath()
 		this.ctx.save();
@@ -317,6 +408,16 @@ class Canvas{
 		this.ctx.stroke();
 		if(f)this.ctx.fill();
 	}
+	/**
+	 * Pinta un Arco
+ 	 * @param {number} x Posición en x
+	 * @param {number} y Posición en x
+	 * @param {number} r Radio
+	 * @param {number} ang Angulo inicial En RADIANES
+	 * @param {number} angf Angulo final EN RADIANES
+	 * @param {string} c Color
+	 * @param {boolean} f Relleno o no relleno por defecto esta en false
+	 */
 	arc(x,y,r,ang=0,angf=Math.PI*2,c,f=false){
 		this.ctx.beginPath()
 		this.ctx.save();
@@ -326,7 +427,16 @@ class Canvas{
 		this.ctx.stroke();
 		if(f)this.ctx.fill();
 	}
-	elipce(x,y,w,h,c,f=false){//inserta una elipce dentro de un rectangulo
+	/**
+	 * Inserta una elipse dentro de un rectángulo
+	 * @param {number} x Posición en x
+	 * @param {number} y Posición en x
+	 * @param {number} w Ancho
+	 * @param {number} h Alto
+	 * @param {string} c Color
+	 * @param {boolean} f Relleno o no relleno por defecto esta en false
+	 */
+	elipce(x,y,w,h,c,f=false){
 		let esc=Math.abs(w/h)
 		this.ctx.save();
 		this.ctx.scale(esc, 1);
@@ -337,7 +447,15 @@ class Canvas{
 		this.ctx.stroke();		
 		if(f)this.ctx.fill();
 	}
-
+	/**
+	 * Pinta una linea
+	 * @param {number} x Posición en x
+	 * @param {number} y Posición en y
+	 * @param {number} xf Posición final en x
+	 * @param {number} yf Posición final en y
+	 * @param {number} w Ancho de la linea
+	 * @param {string} c Color de la linea
+	 */
 	line(x,y,xf,yf,w=3,c){
 		this.ctx.lineWidth=w
 		this.ctx.beginPath();
@@ -349,18 +467,36 @@ class Canvas{
 		this.ctx.restore();
 			
 	}
-
-	rotate(cx,cy,a){//recibe el centro de la figura y el angulo en radiandes podria usar toRand(grados)
-		this.ctx.save();
+	/**
+	 * Rota con respecto al angulo (beta)
+	 * @param {number} cx Centro de la figura en x
+	 * @param {number} cy Centro de la figura en y
+	 * @param {number} a angulo en radianes podría usar toRand(grados)
+	 */
+	rotate(cx,cy,a){
 		this.ctx.translate(cx, cy);
 		this.ctx.rotate(a);
 		this.ctx.translate(-cx, -cy);
 	}
+	/**
+	 * Pinta un Texto
+	 * @param {string} t Texto a Pintar
+	 * @param {number} x Posición x
+	 * @param {number} y Posición y
+	 * @param {number} s Tamaño del texto por defecto es 23
+	 * @param {string} c Color del texto
+	 */
 	text(t,x,y,s=23,c="green"){
 		this.ctx.fillStyle=c;
 		this.ctx.font="bold "+s+"px sans-serif";
 		this.ctx.fillText(t,x,y);
 	}
+	/**
+	 * Establece el tamaño del canvas
+	 * @param {number|null} w ancho 
+	 * @param {number|null} h alto
+	 * @returns "{w,h}" retorna el ancho y alto
+	 */
 	size(w=null,h=null){
 		if(w)this.tag.width=w;
 		if(h)this.tag.height=h;
@@ -368,21 +504,49 @@ class Canvas{
 		this.height=this.tag.height
 		return {w:this.tag.width,h:this.tag.height}
 	}
-	fill(){//canvas en tamaño completo de la pag
+	/**
+	 * Canvas en tamaño completo de la pag
+	 */
+	fill(){
 		this.tag.width=innerWidth;
 		this.tag.height=innerHeight;
 		this.width=this.tag.width
 		this.height=this.tag.height
 		$("body").css({margin:0,padding:0,overflow:"hidden"});
 	}
-	event(e,f){//añade ventos al canvas "click" ,"key" etc; canvas.event("click",myFuncion)
+	/**
+	 * Añade eventos al canvas ; 
+	 * @param {string} e Nombre del evento "click" ,"key" etc
+	 * @param {function} f función que ejecuta el evento canvas.event("click",myFuncion)
+	 * la función recibe como parámetro los datos del evento
+	 */
+	event(e,f){
 		this.$.event(e,f);
 	}
-	removeEvent(e,f){//quita ventos al canvas "click" ,"key" etc; canvas.event("click",myFuncion)
+	/**
+	 * Quita eventos al canvas 
+	 * @param {string} e Nombre del evento "click" ,"key" etc
+	 * @param {function} f función que ejecuta el evento canvas.event("click",myFuncion)
+	 */
+	removeEvent(e,f){
 		this.$.removeEvent(e,f);
 	}
+	/**
+	 * Pinta una imagen
+	 * cutX,cutY,cut... son para recortar,espX,espY son para efecto de espejo
+	 * @param {Image} img Imagen
+	 * @param {number} x Posición en x
+	 * @param {number} y Posición en x
+	 * @param {number} w Ancho
+	 * @param {number} h Alto
+	 * @param {number} cutX Inicio recorte en x
+	 * @param {number} cutY Inicio recorte en y
+	 * @param {number} cutW Ancho del recorte
+	 * @param {number} cutH Alto del recorte
+	 * @param {number} espX 
+	 * @param {number} espY 
+	 */
 	img(img,x,y,w,h,cutX=0,cutY=0,cutW=null,cutH=null,espX=1,espY=1){
-		//maneja imagenes;img:Image,x:Int,y:Int,w:Int,h:Int,cutX,cutY,cut... son para recortar,espX,espY son para efecto de espejo
 		(!cutW)?cutW=img.width:false;
 		(!cutH)?cutH=img.height:false;
 
@@ -394,12 +558,17 @@ class Canvas{
 		this.ctx.drawImage(img,cutX,cutY,cutW,cutH,espX*x+ax,espY*y+ay,w,h);
 		this.ctx.scale(espX,espY);
 	}
-	setResponsive(px,py){//recibe posicion relativa
+	setResponsive(px,py){//recibe posición relativa
 		return [px/this.width,py/this.height]
 	}
-	getResponsive(px,py){//obtiene psicion relativa basada en posiciones absolutas
+	getResponsive(px,py){//obtiene posición relativa basada en posiciones absolutas
 		return [px*this.width,py*this.height]
 	}
+	/**
+	 * Obtiene la posición del mouse incluso si el canvas se re-dimensiona
+	 * @param {event} evt recibe un evento de mouse
+	 * @returns "{x,y}" posiciones exactas del mouse
+	 */
 	getMousePosition(evt){
 		let ClientRect = this.tag.getBoundingClientRect(), 
         	scaleX = this.width / ClientRect.width,
